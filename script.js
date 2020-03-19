31b9b1453be40743cf3a44f245832476cb634204
Artur Stecenko <ar2rstecenk0@gmail.com>
2020-03-19 23:22:03 +0200

    Added functions for cheap tickets search

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ const showCity = (input, list) => {
   list.textContent = '';
 
   if (input.value !== '') {
-
     const filterCity = cities.filter((item) => {
       const lowerCaseCity = item.name.toLowerCase();
       return lowerCaseCity.includes(input.value.toLowerCase());
@@ -47,24 +46,38 @@ const showCity = (input, list) => {
   }
 };
 
-inputCitiesFrom.addEventListener('input', () => {
-  showCity(inputCitiesFrom, dropdownCitiesFrom);
-});
-
-dropdownCitiesFrom.addEventListener('click', (event) => {
+const selectCity = (event, input, list) => {
   const target = event.target;
-
   if (target.tagName.toLowerCase() === 'li') {
-    inputCitiesFrom.value = target.textContent;
-    dropdownCitiesFrom.textContent = '';
+    input.value = target.textContent;
+    list.textContent = '';
   }
-});
+};
+
+const renderTodayCheapTickets = (cheapTickets) => {
+  console.log(cheapTickets);
+};
+
+const renderAllCheapTickets = (cheapTickets) => {
+  console.log(cheapTickets);
+};
+
+const renderCheap = (data, date) => {
+  const allCheapTickets = JSON.parse(data).best_prices;
+
+  const todayCheapTikets = allCheapTickets.filter((item) => {
+    return item.depart_date === date;
+  });
+
+  renderTodayCheapTickets(todayCheapTikets);
+  renderAllCheapTickets(allCheapTickets);
+};
 
 inputCitiesFrom.addEventListener('input', () => {
   showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 
-inputCitiesFrom.addEventListener('input', () => {
+inputCitiesTo.addEventListener('input', () => {
   showCity(inputCitiesTo, dropdownCitiesTo);
 });
 
@@ -74,6 +87,28 @@ dropdownCitiesFrom.addEventListener('click', (event) => {
 
 dropdownCitiesTo.addEventListener('click', (event) => {
   selectCity(event, inputCitiesTo, dropdownCitiesTo);
+});
+
+formSearch.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const cityFrom = cities.find((item) => inputCitiesFrom.value === item.name); 
+  const cityTo   = cities.find((item) => inputCitiesTo.value === item.name); 
+  
+  const formData = {
+    from: cityFrom.code,
+    to:   cityTo.code,
+    when: inputDateDepart.value
+  };
+
+  const requestData = '?depart_date=' + formData.when +
+    '&origin=' + formData.from +
+    '&destination=' + formData.to +
+    '&one_way=true';
+
+  getData(calendar + requestData, (response) => {
+    renderCheap(response, formData.when);
+  });
 });
 
 getData(citiesApi, data => {
